d306cc5c4bb09d7dad714ef31b226a18f0e322ae
CHE-HSIEN JACK LIAO <chli7363@stu.sjcc.edu>
2025-10-16 00:25:05 -0700

    Fix file path so that the app can run with no errors

--- lupitabirthday/src/main/java/com/example/BirthdayExample.java
+++ lupitabirthday/src/main/java/com/example/BirthdayExample.java
@@ -63,7 +63,7 @@ public class BirthdayExample {
      * extra credit module on Files, Directories, and Folders in Canvas.
      */
     String pathToFile =
-      "C:/Users/jerom/Documents/GitHub/class-java/birthday-lupita-lookup-app/lupita/src/main/java/com/example/birthdayOnlyForTesting.json";
+      "/Users/youjia/Documents/GitHub/CS084-Assignment-Lupita-s-Birthday-Look-Up/lupitabirthday/src/main/java/com/example/birthdayOnlyForTesting.json";
 
     // students should change the code below in order to implment their own solution
 
